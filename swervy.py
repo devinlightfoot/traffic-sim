a032ee1ba91c051cf41da37c860f512242621a39
devinlightfoot <euler2171828@gmail.com>
2024-01-13 16:10:47 -0800

    finished inlane NaSch algos

--- swervy.py
+++ swervy.py
@@ -25,12 +25,12 @@ def populateRoad(n, v_max, road):
 # set maximum speed per lane of the road
 v_max = [6, 5, 4]
 # set total number of cars on road
-n = 3
+n = 200
 road = populateRoad(n, v_max, empty_road)
 # probability of  acceleration noise
 p_y = 0.1
 # probability of lane change
-p_x = 0.1
+p_x = 0.25
 # record initial road configuration
 position = np.nonzero(road)
 nonZs = [[]]
@@ -40,7 +40,7 @@ for i, x in enumerate(position[0]):
 # print(nonZs)
 t = 0
 # set duration of simulation
-finish = 3
+finish = 600
 
 # implement timestep update loop
 while t < finish:
@@ -53,44 +53,32 @@ while t < finish:
         lanes[x].append(carArr[1][i])
     if t > 0:
         nonZs.append(carOrdinates)
-    print(nonZs[t])
     for i, pos in enumerate(carOrdinates):
-        #print("initial")
-        #print(tmp[pos[0]][pos[1]]-1)
         # implement NaSch algo for each car
         # will need to implement lane changing updates before lane updates
-        # step 1 (is good)
+        # step 1
         tmp[pos[0]][pos[1]] = min(tmp[pos[0]][pos[1]] + 1, v_max[pos[0]] + 1)
-        #print("step 1")
-        #print(tmp[pos[0]][pos[1]]-1)
         # step 2
         vel = tmp[pos[0]][pos[1]] - 1
         lane_index = lanes[pos[0]].index(pos[1])
-        if lane_index == len(lanes[pos[0]]) - 1 and vel + pos[1] >= len(tmp):
-            d = abs((lanes[pos[0]][0] - lane_index) % (len(tmp) - 1))
-        elif i == len(carOrdinates) - 1:
-            d = abs(
-                (lanes[pos[0]][0] - lane_index) % (len(tmp) - 1)
-                + ((len(tmp) - 1) - lane_index)
-            )
+        # print(lane_index)
+        if len(lanes[pos[0]]) == 1:
+            d = len(tmp[pos[0]])
+        elif len(lanes[pos[0]]) != 1 and lane_index == len(lanes[pos[0]]) - 1 and vel + pos[1] >= len(tmp[pos[0]]):
+            d = abs((lanes[pos[0]][0] - pos[1]) % (len(tmp) - 1))
+            #print("long last")
+        elif len(lanes[pos[0]]) != 1 and lane_index == len(lanes[pos[0]]) - 1:
+            d = abs((lanes[pos[0]][0] - pos[1]) % (len(tmp) - 1) + ((len(tmp) - 1) - pos[1]))
         else:
             d = lanes[pos[0]][lane_index + 1] - pos[1]
-        if abs(d) <= tmp[pos[0]][pos[1]] - 1:
+        if abs(d) <= vel:
             tmp[pos[0]][pos[1]] = max(abs(d), 1)
-        #print("d")
-        #print(d)
-        #print("step 2")
-        #print(tmp[pos[0]][pos[1]]-1)
         # step 3
         if rand.random() <= p_y:
             tmp[pos[0]][pos[1]] = max(tmp[pos[0]][pos[1]] - 1, 1)
-        # print("step 3")
-        # print(tmp[pos[0]][pos[1]])
         # step 4
     for pos in carOrdinates:
         vel = tmp[pos[0]][pos[1]] - 1
-        #print("final")
-        print(vel)
         if pos[1] + vel >= len(tmp[pos[0]]):
             road[pos[0]][(pos[1] + vel) % (len(tmp[pos[0]]) - 1)] = tmp[pos[0]][pos[1]]
             if vel != 0:
