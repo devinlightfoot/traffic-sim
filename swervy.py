0c0ffc4dea4ed5ccc5d7ef4432588ae13929bc98
devinlightfoot <euler2171828@gmail.com>
2024-02-05 15:52:16 -0800

    completed backwards distance headway calculations

--- swervy.py
+++ swervy.py
@@ -58,69 +58,94 @@ while t < finish:
         # implement NaSch algo for each car
         vel = tmp[pos[0]][pos[1]] - 1
         lane_index = lanes[pos[0]].index(pos[1])
+        #calculate distance headway
         if len(lanes[pos[0]]) == 1:
             d = len(tmp[pos[0]])
         elif len(lanes[pos[0]]) != 1 and lane_index == len(lanes[pos[0]]) - 1 and vel + pos[1] >= len(tmp[pos[0]]):
-            d = abs((lanes[pos[0]][0] - pos[1]) % (len(tmp) - 1))
+            d = abs((lanes[pos[0]][0] - pos[1]) % (len(tmp[pos[0]]) - 1))
         elif len(lanes[pos[0]]) != 1 and lane_index == len(lanes[pos[0]]) - 1:
-            d = abs((lanes[pos[0]][0] - pos[1]) % (len(tmp) - 1) + ((len(tmp) - 1) - pos[1]))
+            d = abs((lanes[pos[0]][0] - pos[1]) % (len(tmp[pos[0]]) - 1) + ((len(tmp[pos[0]]) - 1) - pos[1]))
         else:
             d = lanes[pos[0]][lane_index + 1] - pos[1]
-        # will need to implement lane changing updates before lane updates
-        #treat each other lane as a viable target
+        #implement lane changing rules and updates
         tmp_lanes=lanes
         if pos[0]==0:
             if tmp[pos[0]+1][pos[1]]==0:
                 tmp_lanes[pos[0]+1].append(pos[1])
                 tmp_lanes[pos[0]+1].sort()
                 lane_index_r = tmp_lanes[pos[0]+1].index(pos[1])
-                if len(lanes[pos[0]+1]) == 1:
+                if len(tmp_lanes[pos[0]+1]) == 1:
                     d_r = len(tmp[pos[0]+1])
-                elif len(lanes[pos[0]+1]) != 1 and lane_index_r == len(lanes[pos[0]+1]) - 1 and vel + pos[1] >= len(tmp[pos[0]+1]):
-                    d_r = abs((lanes[pos[0]+1][0] - pos[1]) % (len(tmp) - 1))
-                elif len(lanes[pos[0]+1]) != 1 and lane_index_r == len(lanes[pos[0]+1]) - 1:
-                    d_r = abs((lanes[pos[0]+1][0] - pos[1]) % (len(tmp) - 1) + ((len(tmp) - 1) - pos[1]))
+                    d_r_b=len(tmp[pos[0]+1])
+                elif len(tmp_lanes[pos[0]+1]) != 1 and lane_index_r == len(tmp_lanes[pos[0]+1]) - 1 and vel + pos[1] >= len(tmp[pos[0]+1]):
+                    d_r = abs((tmp_lanes[pos[0]+1][0] - pos[1]) % (len(tmp[pos[0]+1]) - 1))
+                elif len(tmp_lanes[pos[0]+1]) != 1 and lane_index_r == len(tmp_lanes[pos[0]+1]) - 1:
+                    d_r = abs((lanes[pos[0]+1][0] - pos[1]) % (len(tmp[pos[0]+1]) - 1) + ((len(tmp) - 1) - pos[1]))
+                elif len(tmp_lanes[pos[0]+1]) != 1 and lane_index_r == 0 and tmp_lanes[pos[0]+1][0] > tmp_lanes[pos[0]+1][len(tmp_lanes[pos[0]+1])-1]:
+                    d_r_b=abs(pos[1]-tmp_lanes[pos[0]+1][len(tmp_lanes[pos[0]+1])-1])
+                elif len(tmp_lanes[pos[0]+1]) != 1 and lane_index_r == 0 and tmp_lanes[pos[0]+1][0] < tmp_lanes[pos[0]+1][len(tmp_lanes[pos[0]+1])-1]:
+                    d_r_b=abs(pos[1]-tmp_lanes[pos[0]+1][len(tmp_lanes[pos[0]+1])-1]) % (len(tmp[pos[0]+1])-1)
                 else:
-                    d_r = lanes[pos[0]+1][lane_index_r + 1] - pos[1]
+                    d_r = tmp_lanes[pos[0]+1][lane_index_r + 1] - pos[1]
+                    d_r_b=pos[1]-tmp_lanes[pos[0]+1][lane_index_r-1]
         elif pos[0]==1:
             if tmp[pos[0]-1][pos[1]]==0:
                 tmp_lanes[pos[0]-1].append(pos[1])
                 tmp_lanes[pos[0]-1].sort()
-                lane_index_r = tmp_lanes[pos[0]-1].index(pos[1])
-                if len(lanes[pos[0]-1]) == 1:
+                lane_index_l = tmp_lanes[pos[0]-1].index(pos[1])
+                if len(tmp_lanes[pos[0]-1]) == 1:
                     d_l = len(tmp[pos[0]-1])
-                elif len(lanes[pos[0]-1]) != 1 and lane_index_r == len(lanes[pos[0]-1]) - 1 and vel + pos[1] >= len(tmp[pos[0]-1]):
-                    d_l = abs((lanes[pos[0]-1][0] - pos[1]) % (len(tmp) - 1))
-                elif len(lanes[pos[0]-1]) != 1 and lane_index_r == len(lanes[pos[0]-1]) - 1:
-                    d_l = abs((lanes[pos[0]-1][0] - pos[1]) % (len(tmp) - 1) + ((len(tmp) - 1) - pos[1]))
+                    d_l_b=len(tmp[pos[0]-1])
+                elif len(tmp_lanes[pos[0]-1]) != 1 and lane_index_l == len(tmp_lanes[pos[0]-1]) - 1 and vel + pos[1] >= len(tmp[pos[0]-1]):
+                    d_l = abs((tmp_lanes[pos[0]-1][0] - pos[1]) % (len(tmp[pos[0]-1]) - 1))
+                elif len(tmp_lanes[pos[0]-1]) != 1 and lane_index_l == len(tmp_lanes[pos[0]-1]) - 1:
+                    d_l = abs((lanes[pos[0]-1][0] - pos[1]) % (len(tmp[pos[0]-1]) - 1) + ((len(tmp) - 1) - pos[1]))
+                elif len(tmp_lanes[pos[0]-1]) != 1 and lane_index_l == 0 and tmp_lanes[pos[0]-1][0] > tmp_lanes[pos[0]-1][len(tmp_lanes[pos[0]-1])-1]:
+                    d_l_b=abs(pos[1]-tmp_lanes[pos[0]-1][len(tmp_lanes[pos[0]-1])-1])
+                elif len(tmp_lanes[pos[0]-1]) != 1 and lane_index_l == 0 and tmp_lanes[pos[0]-1][0] < tmp_lanes[pos[0]-1][len(tmp_lanes[pos[0]-1])-1]:
+                    d_l_b=abs(pos[1]-tmp_lanes[pos[0]-1][len(tmp_lanes[pos[0]-1])-1]) % (len(tmp[pos[0]-1])-1)
                 else:
-                    d_l = lanes[pos[0]-1][lane_index_r + 1] - pos[1] 
+                    d_l = tmp_lanes[pos[0]-1][lane_index_r + 1] - pos[1]
+                    d_l_b=pos[1]-tmp_lanes[pos[0]-1][lane_index_r-1]
             elif tmp[pos[0]+1][pos[1]]==0:
                 tmp_lanes[pos[0]+1].append(pos[1])
                 tmp_lanes[pos[0]+1].sort()
                 lane_index_r = tmp_lanes[pos[0]+1].index(pos[1])
-                if len(lanes[pos[0]+1]) == 1:
+                if len(tmp_lanes[pos[0]+1]) == 1:
                     d_r = len(tmp[pos[0]+1])
-                elif len(lanes[pos[0]+1]) != 1 and lane_index_r == len(lanes[pos[0]+1]) - 1 and vel + pos[1] >= len(tmp[pos[0]+1]):
-                    d_r = abs((lanes[pos[0]+1][0] - pos[1]) % (len(tmp) - 1))
-                elif len(lanes[pos[0]+1]) != 1 and lane_index_r == len(lanes[pos[0]+1]) - 1:
-                    d_r = abs((lanes[pos[0]+1][0] - pos[1]) % (len(tmp) - 1) + ((len(tmp) - 1) - pos[1]))
+                    d_r_b=len(tmp[pos[0]+1])
+                elif len(tmp_lanes[pos[0]+1]) != 1 and lane_index_r == len(tmp_lanes[pos[0]+1]) - 1 and vel + pos[1] >= len(tmp[pos[0]+1]):
+                    d_r = abs((tmp_lanes[pos[0]+1][0] - pos[1]) % (len(tmp[pos[0]+1]) - 1))
+                elif len(tmp_lanes[pos[0]+1]) != 1 and lane_index_r == len(tmp_lanes[pos[0]+1]) - 1:
+                    d_r = abs((lanes[pos[0]+1][0] - pos[1]) % (len(tmp[pos[0]+1]) - 1) + ((len(tmp) - 1) - pos[1]))
+                elif len(tmp_lanes[pos[0]+1]) != 1 and lane_index_r == 0 and tmp_lanes[pos[0]+1][0] > tmp_lanes[pos[0]+1][len(tmp_lanes[pos[0]+1])-1]:
+                    d_r_b=abs(pos[1]-tmp_lanes[pos[0]+1][len(tmp_lanes[pos[0]+1])-1])
+                elif len(tmp_lanes[pos[0]+1]) != 1 and lane_index_r == 0 and tmp_lanes[pos[0]+1][0] < tmp_lanes[pos[0]+1][len(tmp_lanes[pos[0]+1])-1]:
+                    d_r_b=abs(pos[1]-tmp_lanes[pos[0]+1][len(tmp_lanes[pos[0]+1])-1]) % (len(tmp[pos[0]+1])-1)
                 else:
-                    d_r = lanes[pos[0]+1][lane_index_r + 1] - pos[1]
+                    d_r = tmp_lanes[pos[0]+1][lane_index_r + 1] - pos[1]
+                    d_r_b=pos[1]-tmp_lanes[pos[0]+1][lane_index_r-1]
         else:
             if tmp[pos[0]-1][pos[1]]==0:
                 tmp_lanes[pos[0]-1].append(pos[1])
                 tmp_lanes[pos[0]-1].sort()
-                lane_index_r = tmp_lanes[pos[0]-1].index(pos[1])
-                if len(lanes[pos[0]-1]) == 1:
+                lane_index_l = tmp_lanes[pos[0]-1].index(pos[1])
+                if len(tmp_lanes[pos[0]-1]) == 1:
                     d_l = len(tmp[pos[0]-1])
-                elif len(lanes[pos[0]-1]) != 1 and lane_index_r == len(lanes[pos[0]-1]) - 1 and vel + pos[1] >= len(tmp[pos[0]-1]):
-                    d_l = abs((lanes[pos[0]-1][0] - pos[1]) % (len(tmp) - 1))
-                elif len(lanes[pos[0]-1]) != 1 and lane_index_r == len(lanes[pos[0]-1]) - 1:
-                    d_l = abs((lanes[pos[0]-1][0] - pos[1]) % (len(tmp) - 1) + ((len(tmp) - 1) - pos[1]))
+                    d_l_b=len(tmp[pos[0]-1])
+                elif len(tmp_lanes[pos[0]-1]) != 1 and lane_index_l == len(tmp_lanes[pos[0]-1]) - 1 and vel + pos[1] >= len(tmp[pos[0]-1]):
+                    d_l = abs((tmp_lanes[pos[0]-1][0] - pos[1]) % (len(tmp[pos[0]-1]) - 1))
+                elif len(tmp_lanes[pos[0]-1]) != 1 and lane_index_l == len(tmp_lanes[pos[0]-1]) - 1:
+                    d_l = abs((lanes[pos[0]-1][0] - pos[1]) % (len(tmp[pos[0]-1]) - 1) + ((len(tmp) - 1) - pos[1]))
+                elif len(tmp_lanes[pos[0]-1]) != 1 and lane_index_l == 0 and tmp_lanes[pos[0]-1][0] > tmp_lanes[pos[0]-1][len(tmp_lanes[pos[0]-1])-1]:
+                    d_l_b=abs(pos[1]-tmp_lanes[pos[0]-1][len(tmp_lanes[pos[0]-1])-1])
+                elif len(tmp_lanes[pos[0]-1]) != 1 and lane_index_l == 0 and tmp_lanes[pos[0]-1][0] < tmp_lanes[pos[0]-1][len(tmp_lanes[pos[0]-1])-1]:
+                    d_l_b=abs(pos[1]-tmp_lanes[pos[0]-1][len(tmp_lanes[pos[0]-1])-1]) % (len(tmp[pos[0]-1])-1)
                 else:
-                    d_l = lanes[pos[0]-1][lane_index_r + 1] - pos[1]
-        # step 1
+                    d_l = tmp_lanes[pos[0]-1][lane_index_r + 1] - pos[1]
+                    d_l_b=pos[1]-tmp_lanes[pos[0]-1][lane_index_r-1]
+        # implement NaSch algo for each car
+        #step 1
         tmp[pos[0]][pos[1]] = min(tmp[pos[0]][pos[1]] + 1, v_max[pos[0]] + 1)
         # step 2
         if abs(d) <= vel:
